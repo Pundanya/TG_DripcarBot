163d9a11d30bf6e1ad0cd82e5a90fee8fd2910e9
Pundanya <52904727+Pundanya@users.noreply.github.com>
2023-05-18 13:59:10 +0500

    Added video note and voice
    Added video note and voice. also some secret command

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 import requests
+import subprocess
 import random
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
@@ -19,8 +20,52 @@ async def start_command(message: types.Message):
 async def get_car(message: types.Message):
     random_car = random.randint(1, 2038)
     await bot.send_video(message.chat.id, f'https://awesomecars.neocities.org/ver2/{random_car}.mp4')
-    await bot.send_video_note(message.chat.id, f'https://awesomecars.neocities.org/ver2/{random_car}.mp4')
-    await bot.send_voice(message.chat.id, f'https://awesomecars.neocities.org/ver2/{random_car}.mp4')
+
+    resize_mp4(random_car)
+    input_file_mp4 = types.InputFile('output/output.mp4')
+    await bot.send_video_note(message.chat.id, input_file_mp4)
+
+    convert_mp4_to_mp3(random_car)
+    input_file_mp3 = types.InputFile('output/output.mp3')
+    await bot.send_voice(message.chat.id, input_file_mp3)
+
+
+def resize_mp4(random_car):
+    if random_car == 0:
+        input_file = "output/rusi.mp4"
+    else:
+        input_file = f'https://awesomecars.neocities.org/ver2/{random_car}.mp4'
+
+    output_file = 'output/output.mp4'
+
+    command = ['ffmpeg', '-i', input_file, "-s", "240x240", "-y", output_file]
+    subprocess.call(command)
+
+
+def convert_mp4_to_mp3(random_car):
+    if random_car == 0:
+        input_file = "output/rusi.mp4"
+    else:
+        input_file = f'https://awesomecars.neocities.org/ver2/{random_car}.mp4'
+    output_file = 'output/output.mp3'
+
+    command = ['ffmpeg', '-i', input_file, "-y", output_file]
+    subprocess.call(command)
+
+
+@dp.message_handler()
+async def get_car(message: types.Message):
+    if message.text.lower() == "древние русы":
+        rusi = types.InputFile('output/rusi.mp4')
+        await bot.send_video(message.chat.id, rusi)
+
+        resize_mp4(0)
+        input_file_mp4 = types.InputFile('output/output.mp4')
+        await bot.send_video_note(message.chat.id, input_file_mp4)
+
+        convert_mp4_to_mp3(0)
+        input_file_mp3 = types.InputFile('output/output.mp3')
+        await bot.send_voice(message.chat.id, input_file_mp3)
 
 
 async def setup_bot_commands(_):
